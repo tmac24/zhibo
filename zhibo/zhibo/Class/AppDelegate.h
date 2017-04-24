6383dfa7837315c27efa51b4af13858ab8d98521
孙涛 <suntao@suntao.local>
2017-04-24 16:29:32 +0800

    cs

---
+++ zhibo/zhibo/Class/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhibo
+//
+//  Created by 孙涛 on 2017/4/24.
+//  Copyright © 2017年 孙涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

f00915605075aeeb186da0c6e77d8c38b40ba6f4
孙涛 <suntao@suntao.local>
2017-04-24 09:56:25 +0800

    初始化

---
+++ zhibo/zhibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  zhibo
+//
+//  Created by 孙涛 on 2017/4/24.
+//  Copyright © 2017年 孙涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
